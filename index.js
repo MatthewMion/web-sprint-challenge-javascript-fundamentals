653125eb57c2ac9be9aefa0f04a01a5568c212b0
MatthewMion <87820540+ShmoneJones@users.noreply.github.com>
2021-08-20 11:02:27 -0400

    finished all except array section

--- index.js
+++ index.js
@@ -189,7 +189,11 @@ CuboidMaker.prototype.surfaceArea =function(){
   Create an object called cuboid that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid. */
 
-
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5,
+});
 
 
 
@@ -202,9 +206,18 @@ CuboidMaker.prototype.surfaceArea =function(){
 // 🦄🦄🦄 Topic 4: Classes 🦄🦄🦄 //
 //🦄🦄🦄 1. Take your prototypes from above and refactor into class syntax. Please rename your class CuboidMakerTwo and your object cuboidTwo 🦄🦄🦄
 class CuboidMakerTwo{
-
+  constructor(attr){
+  this.length = attr.length;
+  this.width = attr.width;
+  this.height = attr.height;
+  }
+  volume() {
+    return this.length * this.width * this.height;
+  }
+  surfaceArea(){
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
+  }
 }
-
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
 // console.log(cuboidTwo.volume()); // 100
